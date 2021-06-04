2f2d55a54304fb9f760bc126f663c1d38f437b18
Elir25 <elirendiles@gmail.com>
2021-06-04 21:41:35 +0200

    created a json file and use it to show the messages of my app and post a new message

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const { request } = require("express");
 
 const app = express();
 
 app.use(cors());
+
+app.use(express.json())
+//created a new json file which contains some messages
+const messagesArr = require('./messages.json');
 
 const welcomeMessage = {
   id: 0,
@@ -19,6 +24,30 @@ const messages = [welcomeMessage];
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
+
+/**------------ read all the messages ------------------------ */
+app.get("/messages", function (request, response) {
+  
+  response.json(messagesArr)
+});
+
+/**------------ Post a messages ------------------------ */
+
+app.post("/messages", function (request, response) {
+  
+  const body = request.body;
+  
+  const newMessage = {
+    id: messagesArr.length + 1,
+    from: body.from,
+    text: body.text,
+  }
+  messagesArr.push(newMessage);
+
+  response.send(newMessage);
+});
+
+
 
 app.listen(3000, () => {
    console.log("Listening on port 3000")
